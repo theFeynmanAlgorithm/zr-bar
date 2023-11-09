70ef661776396155a7be8ca2c2c8542043341c28
zryan <zwr936@gmail.com>
2023-11-08 21:22:17 -0500

    initial content

---
+++ content/jinja_build.py
@@ -0,0 +1,23 @@
+from staticjinja import Site
+from pathlib import Path
+from yaml import load, FullLoader
+
+_DATA_FOLDER = "data"
+
+def load_folder(path: Path):
+    drinks = []
+    drink_paths = sorted(path.glob('*.yaml'))
+    for file in drink_paths:
+        with open(file) as drink_file:
+            drinks.append(load(drink_file, Loader=FullLoader))
+
+    return drinks
+
+if __name__ == '__main__':
+    drink_context = {
+        'classics': load_folder(Path(_DATA_FOLDER)/'classics'),
+        'guest_favorites': load_folder(Path(_DATA_FOLDER)/'guest_favorites'),
+        'shots': load_folder(Path(_DATA_FOLDER)/'shots'),
+    }
+    site = Site.make_site(contexts=[('index.html', drink_context)])
+    site.render(use_reloader=True)
